d0ff980f8447e7f65050983c1d978813c79595c2
altr <altr@users.noreply.github.com>
2022-05-21 12:27:06 +1000

    updated_api

--- custom_components/uhooair/sensor.py
+++ custom_components/uhooair/sensor.py
@@ -1,32 +1,29 @@
 """
 Support for the uHoo indoor air quality monitor.
-
 For more details about this platform, please refer to the documentation at
 https://github.com/mcclown/home-assistant-custom-components
 """
 
-from datetime import timedelta, datetime
+from datetime import timedelta
 import logging
-import math
 import async_timeout
 import voluptuous as vol
 
 from homeassistant.const import (
     CONF_EMAIL,
     CONF_PASSWORD,
-    CONF_DEVICES,
-    DEVICE_CLASS_HUMIDITY,
-    DEVICE_CLASS_TEMPERATURE,
     TEMP_CELSIUS,
 )
+
+from homeassistant.components.sensor import SensorDeviceClass
+
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
-from homeassistant.util import Throttle, dt
+from homeassistant.util import dt
 
 from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
     DataUpdateCoordinator,
     UpdateFailed,
 )
@@ -50,12 +47,12 @@ DEVICE_CLASS_AIR_PRESSURE = "Air Pressure"
 
 SENSOR_TYPES = {
     "temp": {
-        "device_class": DEVICE_CLASS_TEMPERATURE,
+        "device_class": SensorDeviceClass.TEMPERATURE,
         "unit_of_measurement": TEMP_CELSIUS,
         "icon": "mdi:thermometer",
     },
     "humidity": {
-        "device_class": DEVICE_CLASS_HUMIDITY,
+        "device_class": SensorDeviceClass.HUMIDITY,
         "unit_of_measurement": "%",
         "icon": "mdi:water-percent",
     },
@@ -116,66 +113,70 @@ PLATFORM_SCHEMA = cv.PLATFORM_SCHEMA.extend(
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Connect to the uHoo API and find devices."""
     from pyuhoo import Client
-    from aiohttp import ClientSession
 
+    websession = async_get_clientsession(hass)
     # from pyuhoo.data import get_all_devices
     # from pyuhoo.objects import UhooDev
 
     username = config[CONF_EMAIL]
     password = config[CONF_PASSWORD]
-    async with ClientSession() as websession:
-        client = Client(username, password, websession, debug=True)
 
-        try:
-            await client.login()
-            await client.get_latest_data()
+    try:
 
-            async def async_update_data():
-                """Fetch data from API endpoint.
+        debug = False
+        if _LOGGER.getEffectiveLevel() == logging.DEBUG:
+            debug = True
 
-                This is the place to pre-process the data to lookup tables
-                so entities can quickly look up their data.
-                """
-                try:
-                    # Note: asyncio.TimeoutError and aiohttp.ClientError are already
-                    # handled by the data update coordinator.
-                    async with async_timeout.timeout(10):
-                        return await client.get_latest_data()
-                except Exception as err:
-                    raise UpdateFailed(f"Error communicating with API: {err}")
+        client = Client(username, password, websession, debug=debug)
+        await client.login()
+        await client.get_latest_data()
 
-            coordinator = DataUpdateCoordinator(
-                hass,
-                _LOGGER,
-                # Name of the data. For logging purposes.
-                name="uHoo",
-                update_method=async_update_data,
-                # Polling interval. Will only be polled if there are subscribers.
-                update_interval=timedelta(seconds=5 * 60),
+        async def async_update_data():
+            """Fetch data from API endpoint.
+            This is the place to pre-process the data to lookup tables
+            so entities can quickly look up their data.
+            """
+            try:
+                # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+                # handled by the data update coordinator.
+                async with async_timeout.timeout(10):
+                    return await client.get_latest_data()
+                    # return await client.get_latest_data()
+            except Exception as err:
+                raise UpdateFailed(f"Error communicating with API: {err}")
+
+        coordinator = DataUpdateCoordinator(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="uHoo",
+            update_method=async_update_data,
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=5 * 60),
+        )
+
+        # Fetch initial data so we have data when entities subscribe
+        await coordinator.async_refresh()
+
+        all_devices = []
+        devices = client.get_devices()
+
+        for device in devices.values():
+
+            _LOGGER.debug(
+                f"Uhoo device found: {device.serial_number}\n \n  "
+                + f"Temperatue: {device.temp}\n"
             )
 
-            # Fetch initial data so we have data when entities subscribe
-            await coordinator.async_refresh()
+            for sensor in SENSOR_TYPES:
+                if hasattr(device, sensor):
+                    uhoo_sensor = UhooAirSensor(coordinator, device, sensor)
+                    all_devices.append(uhoo_sensor)
 
-            all_devices = []
-            devices = client.get_devices()
-
-            for device in devices.values():
-
-                _LOGGER.debug(
-                    f"Found: {device.serial_number}\n  {device.timestamp}\n  "
-                    + f"{device.temp} {client.user_settings_temp}\n"
-                )
-
-                for sensor in SENSOR_TYPES:
-                    if hasattr(device, sensor):
-                        uhoo_sensor = UhooAirSensor(coordinator, device, sensor)
-                        all_devices.append(uhoo_sensor)
-
-            async_add_entities(all_devices, True)
-            return
-        except Exception as e:
-            _LOGGER.error("Error: {0}".format(e))
+        async_add_entities(all_devices, True)
+        return
+    except Exception as e:
+        _LOGGER.error("Error: {0}".format(e))
 
     raise PlatformNotReady
 
@@ -219,17 +220,17 @@ class UhooAirSensor(Entity):
         """Return additional attributes."""
         return {}
 
-    # @property
-    # def available(self):
-    #     """Device availability based on the last update timestamp."""
+    @property
+    def available(self):
+        """Device availability based on the last update timestamp."""
 
-    #     last_api_read = self._device.timestamp
+        last_api_read = self._device.timestamp
 
-    #     p_time = dt.parse_datetime(last_api_read)
+        p_time = dt.utc_from_timestamp(last_api_read)
 
-    #     # p_time = datetime.strptime(last_api_read, "%Y-%m-%d %H:%M")
+        # p_time = datetime.strptime(last_api_read, "%Y-%m-%d %H:%M")
 
-    #     return (dt.utcnow() - dt.as_utc(p_time)) < (timedelta(minutes=60))
+        return (dt.utcnow() - dt.as_utc(p_time)) < (timedelta(minutes=60))
 
     @property
     def unique_id(self):
@@ -245,3 +246,13 @@ class UhooAirSensor(Entity):
     def should_poll(self):
         """Should device be polled"""
         return True
+
+    async def async_update(self):
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+                # return await self._coordinator.get_latest_data()
+                return await self._coordinator.async_refresh()
+        except Exception as err:
+            raise UpdateFailed(f"Error communicating with API: {err}")
